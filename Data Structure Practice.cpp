fbdf8f85e11553adfe520d5adf3e84442b4567f7
TaBiZoo <rafik-2015@live.fr>
2025-05-12 00:34:15 +0100

    Ajoutez des fichiers projet.

---
+++ Data Structure Practice.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include"clsDblLinkedList.h"
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+int main()
+{
+    clsDblLinkedList<int> MydblLinkedList;
+
+    MydblLinkedList.InsertAtBeginning(5);
+    MydblLinkedList.InsertAtBeginning(4);
+    MydblLinkedList.InsertAtBeginning(3);
+    MydblLinkedList.InsertAtBeginning(2);
+    MydblLinkedList.InsertAtBeginning(1);
+
+    MydblLinkedList.PrintList();
+
+
+    clsDblLinkedList <int> ::Node* N1 = MydblLinkedList.Find(2);
+    if (N1 != NULL)
+    {
+        cout << "\n Node With Value \"2\ found ^_^\n";
+    }
+    else
+    {
+        cout << "\n Node not found -_-\n";
+    }
+}
+
---
+++ clsDblLinkedList.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+template < typename T>
+class clsDblLinkedList
+{
+
+public:
+
+	class Node
+	{
+	public:
+		T value;
+		Node* next;
+		Node* prev;
+	};
+
+	Node* head = NULL;
+
+	void InsertAtBeginning(T val)
+	{
+		Node* new_node = new Node();
+		new_node->value = val;
+		new_node->prev = NULL;
+		new_node->next = head;
+
+
+		if (head != NULL)
+		{
+			head->prev = new_node;
+		}
+
+		head = new_node;
+	}
+
+	void PrintList()
+	{
+		Node* current = head;
+		if (head == NULL)
+		{
+			cout << "\nList is Empty()";
+		}
+
+		while (current != NULL)
+		{
+			cout << "| " << current->value << " |";
+			current = current->next;
+		}
+		cout << "\n\a";
+	}
+
+	Node* Find(T Val)
+	{
+		Node* current = head;
+
+		while (current != NULL)
+		{
+			if (current->value == Val)
+			{
+				return current;
+			}
+			current = current->next;
+		}
+		return NULL;
+	}
+};
+
