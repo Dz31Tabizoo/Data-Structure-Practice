ec3c783d0f06dd62d104f8dc52b1015a4e35c335
ROUABAH MO RAFIK <160128834+Dz31Tabizoo@users.noreply.github.com>
2025-05-17 12:57:52 +0100

    Update clsMyStack.h

--- clsMyStack.h
+++ clsMyStack.h
@@ -1,5 +1,20 @@
 #pragma once
+#include 'clsDblLinkedList'
+
+template <class T>
 class clsMyStack
 {
+protected:
+clsDblLinkedlist <int> _Mylist;
+
+public:
+
+void Push() 
+{
+   _Mylist.InsertAtBeginning();
+}
+
+
+
 };
 
